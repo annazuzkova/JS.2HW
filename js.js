73252d0e7ee55f4dc963861a946c9a3e1a2d8a47
AnnaZuzkova <famzu2018@gmail.com>
2024-04-08 18:53:29 +0300

    fnljf

---
+++ js.js
@@ -0,0 +1,65 @@
+"use strict";
+// let a1 = 5 + 3;
+// console.log(a1);
+// let a2 = 5 - 3;
+// console.log(a2);
+// let a3 = 5 * 3;
+// console.log(a3);
+// let a4 = 5 / 3;
+// console.log(a4);
+
+// // a5
+
+// let a6 = 5 % 3;
+// alert(a6);
+// let a7 = 3 % 5;
+// alert(a7);
+// let a8 = 5 + "3";
+// alert(a8);
+// let a9 = "5" - 3;
+// alert(a9);
+// let a10 = 75 + "кг";
+// alert(a10);
+
+// const a = "введить число";
+// const b = "введить число";
+// const sum = a + b;
+// console.log(sum);
+
+// flk
+
+const k1 = "5px";
+console.log(Number.parseInt(k1));
+const k2 = "12hjj334";
+console.log(Number.parseInt(k2));
+const k3 = "12.45jhdf12";
+console.log(Number.parseInt(k3));
+const k4 = "okffko";
+console.log(Number.parseInt(k4));
+
+console.log(Number.parseFloat(k1));
+console.log(Number.parseFloat(k2));
+console.log(Number.parseFloat(k3));
+console.log(Number.parseFloat(k4));
+
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+console.log(Math.random() * 19);
+
+const w = "10cm";
+const h = "23cm";
+const s = Number.parseInt(w) * Number.parseInt(h);
+console.log(s);
+
+const h2 = "10m";
+const d = "4m";
+const s2 = Number.parseInt(d) * Number.parseInt(h2);
+console.log(s2);
+
+const mj = 3;
+const bjk = 4;
+const k = 3 ** 2 + 4 ** 2;
+console.log("k у 2 ступені=", k);
+
+("t, f, t ,f");
